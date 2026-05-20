a9622bc12fb7a2067108f2819a007d7d7685ba99
Spiral99s <marqueztf@gmail.com>
2026-05-19 20:55:07 -0400

    Delete JS directory

--- JS/coremegustapt1/main.js
+++
@@ -1,9 +0,0 @@
-let meGusta1 = document.querySelector("#meGusta1");
-let conteo1 = document.querySelector("#conteo1");
-let conteoMegusta1 =0
-
-meGusta1.addEventListener('click', (e) => {
-    e.preventDefault();
-    conteoMegusta1++;
-    conteo1.textContent = conteoMegusta1 + " like(s)";
-});
